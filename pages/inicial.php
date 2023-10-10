e989a9a7b4a9fbd771e29dc1fa2e205db237dcbe
fredkendy <fredkendy9@gmail.com>
2023-10-10 20:31:01 -0300

    Layout usuario comum

---
+++ pages/inicial.php
@@ -0,0 +1,24 @@
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Página Inicial</h5>
+                    <span>Seja bem-vindo a Plataforma EAD</span>
+                </div>
+                <div class="card-block">
+                    <p>
+                        "Somos uma plataforma que, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <p><a href="index.php?p=lojaw_cursos">Clique aqui</a> para conhecer nossos cursos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
